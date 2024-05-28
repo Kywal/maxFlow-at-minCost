788414bb82d5aa6734fb516e5388b224049ada0a
Kywal <emanuelkywal@outlook.com>
2024-05-27 21:39:52 -0400

    refact: graph interface constructor and minor errors

--- graphs.cpp
+++ graphs.cpp
@@ -3,23 +3,24 @@
 
 #include <vector>
 #include <tuple>
-
+#include <initializer_list>
 
 template <typename nodeType, typename structureType>
 class Graphs {
 
-private:
 public:
-    
-    structureType graph;
 
-    bool addNode(nodeType newNode) = 0;
-    bool addEdge(nodeType node1, nodeType node2) = 0;
-    bool removeNode(nodeType node) = 0;
-    bool removeEdge(nodeType node1, nodeType node2) = 0;
+    virtual bool addNode(nodeType newNode) = 0;
+    virtual bool addEdge(nodeType node1, nodeType node2) = 0;
+    virtual bool removeNode(nodeType node) = 0;
+    virtual bool removeEdge(nodeType node1, nodeType node2) = 0;
 
-    void createGraph(structureType graph) = 0;
+    virtual nodeType find(nodeType node) = 0;
 
+    virtual void createGraph(structureType graph) = 0;
+
+    Graphs(structureType graphStructure);
+    Graphs(std::initializer_list<std::initializer_list<nodeType>> graphList);
     Graphs();
     ~Graphs();
 };
