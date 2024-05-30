eb1f7d9b0e23ab9f7746a028894bf15073ece768
Emanuel Kywal <emanuelkywal@outlook.com>
2024-05-29 21:13:40 -0300

    feat: network(capacity,cost) impl with adj matrix (#10)
    * feat: network(capacity,cost) impl with adj matrix
    
    * feat,refact: source,terminal ID - remove addNode()
    
    * refact: addArc return type to cap_cost pair

---
+++ network.cpp
@@ -0,0 +1,35 @@
+#ifndef network
+#define network
+
+#include <utility>
+
+class Network {
+
+typedef std::pair<int,int> cap_cost;
+
+private:
+    cap_cost** adjacencyMatrix;
+    int nodeAmount = 0;
+    int arcAmount = 0;
+
+public:
+    int sourceID, terminalID;
+        
+    cap_cost addArc(int src, int dest, int capacity, int cost) {
+        return adjacencyMatrix[src][dest] = {capacity, cost};     
+    }
+
+    Network(int nodeAmount, int arcAmount) {
+        this->nodeAmount = nodeAmount;
+        this->arcAmount = arcAmount;
+
+        adjacencyMatrix = new cap_cost*[nodeAmount];
+    }
+
+    ~Network() {
+        delete []* adjacencyMatrix;
+    }
+
+};
+
+#endif
