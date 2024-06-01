078bcbef377c9b58b59b69e37c94489895137ec0
Kywal <emanuelkywal@outlook.com>
2024-05-31 21:06:50 -0400

    feat: organize project in model,view,tests and utils structure

---
+++ utils/testFilter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "model/network.cpp"
+#include "tests/TestsInput.cpp"
+
+typedef std::string string;
+
+
+int main() {
+
+    return 0;
+}
