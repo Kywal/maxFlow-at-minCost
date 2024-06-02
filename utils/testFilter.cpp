26b4caebd778154fe1008fa9fbcb4b8bdcb5fdff
Kywal <emanuelkywal@outlook.com>
2024-06-02 00:06:12 -0400

    feat: testFilte.cppr complete code

--- utils/testFilter.cpp
+++ utils/testFilter.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "model/network.cpp"
-#include "tests/TestsInput.cpp"
+#include <filesystem>
 
-typedef std::string string;
+#include "../model/network.cpp"
+#include "../tests/TestsInput.cpp"
+#include "../tests/TestsOutput.cpp"
 
+using directory = std::filesystem::recursive_directory_iterator;
+using namespace std;
 
 int main() {
+
+    TestsInput testIn;
+    TestsOutput testOut;
+    string folderPath = "Lemon_Dataset/";
+
+    for(const auto& file : directory(folderPath)) {
+        
+        for (size_t i = 20; i <= 100; i = i + 20) {
+
+            Network* graph = testIn.readGraphFile(file.path(), i);
+            
+            string filePath(file.path().filename());
+            string graphFileName = "testCases/" + filePath + to_string(i) + ".txt";
+            testOut.writeNetworkFile(graphFileName,*graph);
+            graph = nullptr;
+        }
+     
+    }
 
     return 0;
 }
