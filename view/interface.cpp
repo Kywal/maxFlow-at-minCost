a2743079211f3835ed7cf611eda2b25d1d5ab40a
Kywal <emanuelkywal@outlook.com>
2024-06-02 00:07:34 -0400

    feat: testCases filtered by testFilter.cpp

--- view/interface.cpp
+++ view/interface.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include "TestsInput.cpp"
-#include "model/network.cpp"
 
 using namespace std;
 
 int main() {
 
-    TestsInput test;
-
-    Network *graph = test.readGraphFile("test.txt",100);
-    
-    graph->printAdjMatrix();
+    cout << "I'm testing makefile!!!\n";
 
     return 0;
 }
